16f32400b69c0f810da0a2723e9ee85ecc5d5a22
MTW-EN01 <mtw-en01@rmc.co.th>
2026-04-08 16:02:26 +0700

    080426-1602

--- plan.js
+++ plan.js
@@ -265,15 +265,22 @@ approve user✓
 Bug 
 -DocNumber ซ้ำกัน (ตอนนี้เมื่อ send memo แล้ว docNumber ไม่ gen ใหม่)
 
+
+
 08/04/26
+
+-เพิ่ม upload pic ใน memo ได้ (cloudinary) ✓
+-แก้ไขให้ส่ง memo ได้หลาย user ✓
+-แก้ไข log ไม่อัพเดท✓
+
+
+-แก้ไข quick access
+-คนที่ approve memo ให้ส่ง memo โดยที่ไม่ต้องอนุมัติ
+-ทดสอบ setting approver by subdepartment
+
 -แก้ layout หน้า mobile
 -add function | search box
 -add function | icon setting topbar
 
--ทดสอบ setting approver by subdepartment
--คนที่ approve memo ให้ส่ง memo โดยที่ไม่ต้องอนุมัติ
--เพิ่ม upload pic ใน memo ได้ (cloudinary) ✓
--แก้ไขให้ส่ง memo ได้หลาย user ✓
--แก้ไข log ไม่อัพเดท
 */
 
